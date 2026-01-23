ceacc58daef5a89424e7e7ace3b397e33adc71b9
Mohammed Souban <md.souban@gmail.com>
2026-01-23 22:16:08 +0530

    Update routes with invoice page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import Dashboard from "./pages/Dashboard.jsx";
-import Menu from "./pages/Menu.jsx";
-import Billing from "./pages/Billing.jsx";
-import Report from "./pages/Report.jsx";
-import Invoice from "./pages/Invoice.jsx";
+import Dashboard from "./pages/Dashboard";
+import Menu from "./pages/Menu";
+import Billing from "./pages/Billing";
+import Report from "./pages/Report";
+import Invoice from "./pages/Invoice";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Home */}
         <Route path="/" element={<Dashboard />} />
+
+        {/* Pages */}
         <Route path="/menu" element={<Menu />} />
         <Route path="/billing" element={<Billing />} />
         <Route path="/report" element={<Report />} />
+
+        {/* ✅ Invoice page for WhatsApp link */}
         <Route path="/invoice/:id" element={<Invoice />} />
-        <Route path="*" element={<Dashboard />} />
+
+        {/* If wrong URL */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
